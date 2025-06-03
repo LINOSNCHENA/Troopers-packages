d482645b287b2820f4afc6d950196b91aba03eae
Marvin Nchena <linosnchena@gmail.com>
2025-06-03 09:23:19 +0200

    pages

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_VERSION = exports.API_URL = void 0;
-// index.ts
-exports.API_URL = 'https://tapi.example.com';
-exports.API_VERSION = 'https://troopers-money-matters.netlify.app/';
