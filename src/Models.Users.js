4fa39f153cb8333cf6a0675952a5bf543439e73d
Marvin Nchena <linosnchena@gmail.com>
2025-06-19 18:24:01 +0200

    developed data

--- src/Models.Users.js
+++ src/Models.Users.js
@@ -1,4 +1,6 @@
 "use strict";
 // =====================|SEVEN|===============================|1/7|
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.developers = void 0;
+exports.developers = "Developer : TroopersCOder: Version 1.0.1";
 // =======================|SEVEN|====================================[1/7]
